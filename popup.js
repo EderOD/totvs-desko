51b48b7c559c40946ee2bf8f0bb4ba2d006c155c
Eder Dias <37964092+EderOD@users.noreply.github.com>
2022-04-10 12:13:48 -0300

    Read tab content to execute command

--- popup.js
+++ popup.js
@@ -1,13 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(){
     
 document.querySelector('#btn').addEventListener('click',function(){
-    let text =  document.querySelector('#texto').value;
-
-    document.querySelector("#result").innerHTML = upper(text);
+    chrome.tabs.executeScript({
+        code: 'document.getElementsByClassName("my-booking__place-name")[0].innerText' //argument here is a string but function.toString() returns function's code
+    }, (results) => {
+        let text = results
+        document.querySelector("#result").innerHTML = concatCode(text);
+        return results
+    });
 })
 
-function upper(text){
-    return text.toUpperCase();
+function concatCode(text){
+    if(text[0] == null)
+        return 'EST.BH.MG.2/' + document.querySelector('#texto').value
+    else
+        return 'EST.BH.MG.2/' + text
 }
 
 })
